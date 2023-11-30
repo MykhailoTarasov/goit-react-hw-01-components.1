57b86a56664ef382ce047cdb39cf397a57f44e1a
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-11-30 14:51:42 +0200

    render done

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,33 @@
+export default function Profile({
+  userName,
+  tag,
+  location,
+  avatar,
+  stats: {follovers, views, likes }
+}) {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt={userName} class="avatar" />
+        <p class="name">{userName}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">{follovers}</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">{views}</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">{likes}</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/TransactionsHistory/TransactionsHistory.js
@@ -0,0 +1,23 @@
+export default function TransactionHistory({ items }) {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ type, amount, currency }) => (
+          <tr>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
